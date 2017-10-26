379e59d19514a7e2d00295481318f37f91a6fc2c
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2017-10-26 13:12:27 +0400

    Form Input for Task entry
    New form input was added to facilitate task entry. An event handler is
    attached to form Submit and that is catched similar to helper function.
    Each such events are a par tof dictionary and event handler becomes the
    key.

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -7,6 +7,29 @@ import './body.html';
 
 Template.body.helpers({
     tasks(){
-      return Tasks.find({});
+      return Tasks.find({}, { sort: { createdAt: -1 } });
     },
+});
+
+
+Template.body.events({
+  
+  'submit .new-task'(event) {
+    //prevent browser form submit default 
+    event.preventDefault();
+    
+    // Get value frpm form element
+    const target = event.target;
+    const text = target.text.value;
+  
+    // Insert a task into collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+    
+    //Clear form
+    target.text.value = '';
+  
+  },
 });
